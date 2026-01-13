e5454738bdce20c49e4b5fb5eabd6ab048699440
songKimvisal <songkimvisal21@gmail.com>
2026-01-13 23:00:16 +0700

    add the dummy data for the subcategories

---
+++ src/constants/CategoryData.ts
@@ -0,0 +1,75 @@
+export const CATEGORY_MAP: Record<string, { sub: string[] }> = {
+  "1": {
+    sub: ["Phone", "Tablet", "Smart Watch", "Phone Accessories", "other"],
+  },
+  "2": {
+    sub: [
+      "Car",
+      "Bicycle",
+      "Motorcycle",
+      "Lorries",
+      "Tractors",
+      "Tuk Tuk & Remork",
+      "Parts & Accessories",
+      "Vehicles For Rent",
+      "other",
+    ],
+  },
+  "3": {
+    sub: ["Skin Care", "Hair Care", "Makeup", "Natural & Organic", "other"],
+  },
+  "4": {
+    sub: [
+      "Tables & Desks",
+      "Chairs & Sofas",
+      "Wardrobes & Cabinets",
+      "Shelves & Drawers",
+      "Beds & Mattresses",
+      "Curtain & Carpet",
+      "Kitchenware",
+      "Household Items",
+      "Handicrafts-Paintings",
+    ],
+  },
+  "5": {
+    sub: [
+      "Women's Fashion",
+      "Men's Fashion",
+      "Baby & Kids",
+      "Fashion Accessories",
+      "other",
+    ],
+  },
+  "6": {
+    sub: [
+      "Laptop",
+      "Desktop",
+      "All in One",
+      "Monitor Printer & Scanner",
+      "Parts & Accessories",
+    ],
+  },
+  "7": { sub: ["Land", "House", "Condo", "Apartment", "Commercial"] },
+  "8": {
+    sub: [
+      "Washing Machines",
+      "Fridges",
+      "Air Conditioning",
+      "Tools",
+      "Machinery",
+      "Security Camera",
+      "TVs & Audio",
+      "Video games",
+      "Toys",
+    ],
+  },
+};
+
+export const PROVINCES = [
+  "All Cambodia",
+  "Phnom Penh",
+  "Siem Reap",
+  "Sihanoukville",
+  "Battambang",
+  "Kampot",
+];
